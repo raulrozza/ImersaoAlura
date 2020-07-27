e225b71936f36036ad631f39743528b38a4107a1
Raul Rosá <raul.hmetal@gmail.com>
2020-07-27 17:12:42 -0300

    Transcribed the last component to Typescript

---
+++ src/components/Carousel/components/VideoCard/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { VideoCardContainer } from './styles';
+
+function getYouTubeId(youtubeURL: string) {
+    return youtubeURL.replace(
+        /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/,
+        '$7',
+    );
+}
+
+interface IProps {
+    videoTitle: string;
+    videoURL: string;
+    categoryColor: string;
+}
+
+const VideoCard: React.FC = ({ videoTitle, videoURL, categoryColor }) => {
+    const image = `https://img.youtube.com/vi/${getYouTubeId(
+        videoURL,
+    )}/hqdefault.jpg`;
+    return (
+        <VideoCardContainer
+            url={image}
+            href={videoURL}
+            target="_blank"
+            style={{ borderColor: categoryColor || 'red' }}
+            title={videoTitle}
+        />
+    );
+};
+
+VideoCard.propTypes = {
+    videoTitle: PropTypes.string.isRequired,
+    videoURL: PropTypes.string.isRequired,
+    categoryColor: PropTypes.string.isRequired,
+};
+
+export default VideoCard;
